106cba7661106be45820eef40e4feb90ff14672e
[Manuel] <[manuelaguiargamez@gmail.com]>
2022-03-09 16:02:07 -0600

    09MAR22

--- database/migrations/2022_03_09_205130_create_access_tokens_table.php
+++ database/migrations/2022_03_09_205130_create_access_tokens_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateAccessTokensTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('access_tokens', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->rememberToken();
+
+            $table->foreignId('user_id')->constrained();
+            $table->unsignedBigInteger('service_id');
+            $table->text('access_token');
+            $table->text('refresh_token');
+            $table->dateTime('expires_at');
+
             $table->timestamps();
         });
     }
@@ -29,6 +33,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('access_tokens');
     }
 }
